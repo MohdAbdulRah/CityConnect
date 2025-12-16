1d5f7eb9fb8312695b686585267d3037fcc22ffb
Mohd Abdul Rahman <mohdabdulrahman510@gmail.com>
2025-12-16 13:47:42 +0530

    Add files via upload

---
+++ frontend/config.js
@@ -0,0 +1,7 @@
+
+module.exports = {
+  API_BASE_URL: "https://nontrailing-multiphase-lida.ngrok-free.dev",   // backend
+  CLOUDINARY_CLOUD_NAME : "drmuvz4j3",
+  CLOUDINARY_API_KEY : "544626382446718",
+  CLOUDINARY_API_SECRET : "ppeM6jtMfdkOxplhh3cZXhVPQsE"
+};
