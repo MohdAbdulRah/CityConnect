445b5e271093fb3ec930053c2ce602025b516c89
Mohd Abdul Rahman <mohdabdulrahman510@gmail.com>
2025-12-24 10:09:25 +0530

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,10 +1,41 @@
 const express = require("express");
 const { signup, verifyEmail, resendOtp,login } = require("../controllers/authController");
 const router = express.Router();
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const nodemailer = require("nodemailer");
 
 router.post("/signup", signup);
 router.post("/verify-email", verifyEmail);
 router.post("/resend-otp", resendOtp);
 router.post("/login", login);
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false
+  }
+});
+
+app.get("/test-mail", async (req, res) => {
+  try {
+    await transporter.sendMail({
+      from: process.env.MAIL_USER,
+      to: process.env.MAIL_USER,
+      subject: "Test Mail",
+      text: "Hello from Render"
+    });
+    res.send("Mail sent");
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
+});
+
 
 module.exports = router;
