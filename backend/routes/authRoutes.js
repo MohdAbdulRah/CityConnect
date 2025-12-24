eb84b1a424e1847b9b724e44a982c0cc8e6a8fc2
Mohd Abdul Rahman <mohdabdulrahman510@gmail.com>
2025-12-24 10:39:02 +0530

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,39 +1,30 @@
 const express = require("express");
-const { signup, verifyEmail, resendOtp,login } = require("../controllers/authController");
+const { signup, verifyEmail, resendOtp, login } = require("../controllers/authController");
+const transporter = require("../config/mail");
+
 const router = express.Router();
-const User = require("../models/User");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const nodemailer = require("nodemailer");
 
 router.post("/signup", signup);
 router.post("/verify-email", verifyEmail);
 router.post("/resend-otp", resendOtp);
 router.post("/login", login);
-const transporter = nodemailer.createTransport({
-  host: "smtp-relay.brevo.com",
-  port: 587,
-  secure: false,
-  auth: {
-    user: "apikey",
-    pass: process.env.BREVO_API_KEY,
-  },
-});
 
 router.post("/test-mail", async (req, res) => {
-  const {email} = req.body
+  const { email } = req.body;
+
   try {
     await transporter.sendMail({
-  from: `"CityConnect" <${process.env.MAIL_USER}>`,
-  to: email,
-  subject: "Your OTP Code",
-  html: `<h2>Your OTP Code:</h2><h1>12345</h1>`,
-});
-    res.send("Mail sent");
+      from: `"CityConnect" <${process.env.MAIL_USER}>`,
+      to: email,
+      subject: "Your OTP Code",
+      html: `<h2>Your OTP Code:</h2><h1>12345</h1>`,
+    });
+
+    res.json({ success: true, message: "Mail sent" });
   } catch (e) {
-    res.status(500).send(e.message);
+    console.log("MAIL ERROR:", e.message);
+    res.status(500).json({ message: "Mail failed" });
   }
 });
-
 
 module.exports = router;
